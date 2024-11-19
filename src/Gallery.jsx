fa945c9e88f9d4ee28466cb8f0520b2a4c3a7824
Christian Schmidt <cjschmidt33@gmail.com>
2024-11-18 21:59:38 -0500

    Add the API tour information to the page

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,9 +3,34 @@ import React, { useState, useEffect } from 'react';
 function Gallery() {
     const [tours, setTours] = useState([]); //sets tours as our initial variable, with the default tours being blank
 
+    const notInterested = () => {
+
+    }
+
     useEffect(() => {
-        fetch('https://course-api.com/react-tours-project') //fetch tours from API
+        fetch('https://www.course-api.com/react-tours-project') //fetch tours from API
             .then(response => response.json()) //turn API into JSON format
-            .then(data => setTours(data)); //add tours to the tours array
-    }, []);
+            .then(data => setTours(data)) //add tours to the tours array
+            .catch(error => console.error('Error Fetching Tours:', error))
+        }, []);
+
+    return (
+        <div>
+            <h2>Tours</h2>
+            <ul>
+                {tours.map(tour => (
+                    <li key={tour.id}>
+                        {tour.name} - ${tour.price}
+                        <p>{tour.info}</p>
+                        <img src={tour.image}></img>
+                        <br></br>
+                        <button onClick={notInterested}>Not Interested</button>
+                        <p></p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
+
+export default Gallery;
