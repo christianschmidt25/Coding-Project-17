675f221b9efbffb117c02f872f74aee9f0940d98
Christian Schmidt <cjschmidt33@gmail.com>
2024-11-19 15:41:41 -0500

    Add a Loading Tours Now... result when tours are being fetched

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function Gallery() {
     const [tours, setTours] = useState([]); //sets tours as our initial variable, with the default tours being blank
+    const [loading, setLoading] = useState(true); //adds a loading phase while fetching tours
 
     const notInterested = (id) => {
         setTours((prevTours) => prevTours.filter((tour) => tour.id !== id)) //when selecting this button underneath the tour (associated by ID), it will filter out and remove from the tour list.
@@ -17,11 +18,22 @@ function Gallery() {
 
 
     useEffect(() => {
+        setLoading(true); //displays loading message at this time
         fetch('https://www.course-api.com/react-tours-project') //fetch tours from API
             .then(response => response.json()) //turn API into JSON format
-            .then(data => setTours(data)) //add tours to the tours array
-            .catch(error => console.error('Error Fetching Tours:', error))
+            .then(data => {
+                setTours(data.map((tour) => ({ ...tour, expanded: false}))); //add tours to the tours array 
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error Fetching Tours:', error);
+                setLoading(false);
+            });
         }, []);
+
+    if (loading) { //this is what will display when fetching tours
+        return <div>Loading Tours Now...</div>
+    }
 
     return (
         <div>
